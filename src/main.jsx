6979ae1100be41d43f1258cb2ee9db8079104185
nara <parknara0213@gmail.com>
2024-04-26 17:58:35 +0900

    240426 updating contacts with formdata

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import Root, {
 } from "./routes/root";
 import ErrorPage from "./error-page";
 import Contact, { loader as contactLoader } from "./routes/Contact";
-import EditContact from "./routes/edit";
+import EditContact, { action as editAction } from "./routes/edit";
 
 const router = createBrowserRouter([
   {
@@ -29,6 +29,7 @@ const router = createBrowserRouter([
         path: "contacts/:contactId/edit",
         element: <EditContact />,
         loader: contactLoader,
+        action: editAction,
       },
     ],
   },
