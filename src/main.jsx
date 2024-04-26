5a7a7c8825f41f9f90b56744d2738ac7fad7d261
nara <parknara0213@gmail.com>
2024-04-26 16:11:18 +0900

    240426 create contacts

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,10 @@ import * as ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import "./index.css";
-import Root, { loader as rootLoader } from "./routes/root";
+import Root, {
+  loader as rootLoader,
+  action as rootAction,
+} from "./routes/root";
 import ErrorPage from "./error-page";
 import Contact from "./routes/Contact";
 
@@ -14,6 +17,7 @@ const router = createBrowserRouter([
     element: <Root />,
     errorElement: <ErrorPage />,
     loader: rootLoader,
+    action: rootAction,
     children: [
       {
         path: "contacts/:contactId",
