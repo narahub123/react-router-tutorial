da7090a76ae0dba434d98cc73355e9799eeda12d
nara <parknara0213@gmail.com>
2024-04-26 21:41:09 +0900

    240426 Cancel Button

--- src/routes/edit.jsx
+++ src/routes/edit.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Form, useLoaderData, redirect } from "react-router-dom";
+import { Form, useLoaderData, redirect, useNavigate } from "react-router-dom";
 import { updateContact } from "../contacts";
 
 export async function action({ request, params }) {
@@ -11,6 +11,7 @@ export async function action({ request, params }) {
 
 const EditContact = () => {
   const { contact } = useLoaderData();
+  const navigate = useNavigate();
 
   return (
     <Form method="post" id="contact-form">
@@ -56,7 +57,14 @@ const EditContact = () => {
       </label>
       <p>
         <button type="submit">Save</button>
-        <button type="button">Cancel</button>
+        <button
+          type="button"
+          onClick={() => {
+            navigate(-1);
+          }}
+        >
+          Cancel
+        </button>
       </p>
     </Form>
   );
