23d2daba07f850bff5fd736cb541dbba78feafd1
nara <parknara0213@gmail.com>
2024-04-29 15:16:41 +0900

    240429 the contact route ui

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,17 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import Root from "./routes/root";
 import ErrorPage from "./ErrorPage";
+import Contact from "./routes/Contact";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+  },
+  {
+    path: "/contacts/:contactId",
+    element: <Contact />,
   },
 ]);
 
