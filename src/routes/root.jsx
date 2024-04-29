0f175d7bce1c318a483ccdc182534426715dc68b
nara <parknara0213@gmail.com>
2024-04-29 20:50:02 +0900

    240429 GET submissions with client side routing

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -9,8 +9,10 @@ import {
 } from "react-router-dom";
 import { getContacts, createContact } from "../contacts";
 
-export async function loader() {
-  const contacts = await getContacts();
+export async function loader({ request }) {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q");
+  const contacts = await getContacts(q);
   return { contacts };
 }
 
@@ -27,7 +29,7 @@ const Root = () => {
       <div id="sidebar">
         <h1>React Router Contacts</h1>
         <div>
-          <form id="search-form" role="search">
+          <Form id="search-form" role="search">
             <input
               type="search"
               id="q"
@@ -37,7 +39,7 @@ const Root = () => {
             />
             <div id="search-spinner" aria-hidden hidden={true} />
             <div className="sr-only" aria-live="polite"></div>
-          </form>
+          </Form>
           <Form method="post">
             <button type="submit">New</button>
           </Form>
